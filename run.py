043ba9a0a3e839984a3d21e34e7afd40ada8405b
CristianEstupinan <cristian.estupinan@spy0045.com>
2017-05-10 18:05:36 +0100

    Initial commit

---
+++ run.py
@@ -0,0 +1,26 @@
+# Search methods
+
+import search
+
+ab = search.GPSProblem('A', 'O', search.romania)
+
+print "Anchura:"
+print "Nodos expandidos:"
+print search.breadth_first_graph_search(ab).path()
+print "Profundidad:"
+print "Nodos expandidos:"
+print search.depth_first_graph_search(ab).path()
+print "Ramificacion y acotacion:"
+print "Nodos expandidos:"
+print search.ramificacion_acotacion(ab).path()
+print "Ramificacion y acotacion con subestimacion:"
+print "Nodos expandidos:"
+print search.ramificacion_acotacion_subestimacion(ab).path()
+
+#print search.iterative_deepening_search(ab).path()
+#print search.depth_limited_search(ab).path()
+#print search.astar_search(ab).path()
+
+# Result:
+# [<Node B>, <Node P>, <Node R>, <Node S>, <Node A>] : 101 + 97 + 80 + 140 = 418
+# [<Node B>, <Node F>, <Node S>, <Node A>] : 211 + 99 + 140 = 450
